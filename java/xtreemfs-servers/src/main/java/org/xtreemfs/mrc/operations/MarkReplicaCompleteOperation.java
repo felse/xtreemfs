fa7b6f2e18246240404dbb1eed45ef62b2791617
Felix <seibert@zib.de>
2018-02-08 14:18:54 +0100

    adding markReplicaComplete to RPC interfaces, corresponding MRCOperation with dummy implementation, and a call from ObjectDissemination when replication is done

---
+++ java/xtreemfs-servers/src/main/java/org/xtreemfs/mrc/operations/MarkReplicaCompleteOperation.java
@@ -0,0 +1,26 @@
+package org.xtreemfs.mrc.operations;
+
+import org.xtreemfs.foundation.logging.Logging;
+import org.xtreemfs.mrc.MRCRequest;
+import org.xtreemfs.mrc.MRCRequestDispatcher;
+import org.xtreemfs.pbrpc.generatedinterfaces.MRC.xtreemfs_replica_mark_completeRequest;
+
+public class MarkReplicaCompleteOperation extends MRCOperation {
+
+
+    public MarkReplicaCompleteOperation(MRCRequestDispatcher master) {
+        super(master);
+    }
+
+    @Override
+    public void startRequest(MRCRequest rq) throws Throwable {
+        final xtreemfs_replica_mark_completeRequest rqArgs =
+                (xtreemfs_replica_mark_completeRequest) rq.getRequestArgs();
+
+        if (Logging.isDebug()) {
+            Logging.logMessage(Logging.LEVEL_DEBUG, this,
+                               "Replica on OSD %s of file with id %s is now marked as complete",
+                               rqArgs.getFileId(), rqArgs.getOsdUuid());
+        }
+    }
+}
