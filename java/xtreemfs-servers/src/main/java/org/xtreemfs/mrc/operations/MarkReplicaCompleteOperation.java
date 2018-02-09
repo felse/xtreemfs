afd9b53a81598a996a59402954a392695a3f4ce9
Felix <seibert@zib.de>
2018-02-09 11:22:24 +0100

    implementing the mark replica complete operation

--- java/xtreemfs-servers/src/main/java/org/xtreemfs/mrc/operations/MarkReplicaCompleteOperation.java
+++ java/xtreemfs-servers/src/main/java/org/xtreemfs/mrc/operations/MarkReplicaCompleteOperation.java
@@ -3,16 +3,25 @@ package org.xtreemfs.mrc.operations;
 import org.xtreemfs.foundation.logging.Logging;
 import org.xtreemfs.mrc.MRCRequest;
 import org.xtreemfs.mrc.MRCRequestDispatcher;
+import org.xtreemfs.mrc.database.AtomicDBUpdate;
 import org.xtreemfs.mrc.database.StorageManager;
 import org.xtreemfs.mrc.database.VolumeManager;
-import org.xtreemfs.mrc.metadata.XLoc;
+import org.xtreemfs.mrc.metadata.XLocList;
 import org.xtreemfs.mrc.utils.MRCHelper;
-import org.xtreemfs.osd.storage.FileMetadata;
 import org.xtreemfs.pbrpc.generatedinterfaces.MRC
         .xtreemfs_replica_mark_completeRequest;
 
 import org.xtreemfs.pbrpc.generatedinterfaces.Common.emptyResponse;
 
+/**
+ * MRC operation to mark a previously added replica as a complete replica.
+ * This operation should only be used by an OSD when it fetched and stored all
+ * objects locally (e.g. when the replication completed successfully).
+ * <p>
+ * This operation is only necessary for read-only replication.
+ * Probably we should throw some exception if the file's replication policy is
+ * not read-only.
+ */
 public class MarkReplicaCompleteOperation extends MRCOperation {
 
 
@@ -25,14 +34,15 @@ public class MarkReplicaCompleteOperation extends MRCOperation {
         final xtreemfs_replica_mark_completeRequest rqArgs =
                 (xtreemfs_replica_mark_completeRequest) rq.getRequestArgs();
 
+        String fileID = rqArgs.getFileId();
+        String osdWithCompleteReplica = rqArgs.getOsdUuid();
+
         if (Logging.isDebug()) {
             Logging.logMessage(Logging.LEVEL_DEBUG, this,
                                "Replica on OSD %s of file with id %s is now " +
                                        "marked as complete",
-                               rqArgs.getOsdUuid(), rqArgs.getFileId());
+                               osdWithCompleteReplica, fileID);
         }
-
-        String fileID = rqArgs.getFileId();
 
         MRCHelper.GlobalFileIdResolver idResolver = new MRCHelper
                 .GlobalFileIdResolver(fileID);
@@ -45,26 +55,59 @@ public class MarkReplicaCompleteOperation extends MRCOperation {
         org.xtreemfs.mrc.metadata.FileMetadata fileMetadata =
                 storageManager.getMetadata(idResolver.getLocalFileId());
 
-        StringBuilder replicaInfo = new StringBuilder();
+        XLocList xLocList = fileMetadata.getXLocList();
 
         if (Logging.isDebug()) {
-            for (int i = 0; i < fileMetadata.getXLocList().getReplicaCount(); i++) {
-
-                replicaInfo
-                        .append("replica ")
-                        .append(i)
-                        .append(" osd: ")
-                        .append(fileMetadata.getXLocList().getReplica(i)
-                                        .getOSD(0))
-                        .append(" replication flags: ")
-                        .append(fileMetadata.getXLocList().getReplica(i).getReplicationFlags())
-                        .append("\n");
-            }
-
-            Logging.logMessage(Logging.LEVEL_DEBUG, this,
-                               replicaInfo.toString());
+            logXLocInfo(xLocList);
         }
 
+//        // assume that there is exactly one entry in the xLocList for
+//        // OSD osdWithCompleteReplica
+//        XLoc replicaToBeUpdated = null;
+//        for (int i = 0; i < xLocList.getReplicaCount(); i++) {
+//            if (osdWithCompleteReplica
+//                    .equals(xLocList.getReplica(i).getOSD(i))) {
+//                replicaToBeUpdated = xLocList.getReplica(i);
+//                break;
+//            }
+//        }
+//
+//        if (replicaToBeUpdated == null) {
+//            // TODO throw some exception?
+//            // or just ignore?
+//            return;
+//        }
+
+        // it seems that the MRCHelper contains code to mark a replica as
+        // complete in the MetaDataDataBase
+        AtomicDBUpdate atomicDBUpdate =
+                storageManager.createAtomicDBUpdate(master, rq);
+        MRCHelper.setSysAttrValue(master,
+                                  storageManager,
+                                  -1,
+                                  fileMetadata,
+                                  MRCHelper.SysAttrs.mark_replica_complete.toString(),
+                                  osdWithCompleteReplica,
+                                  atomicDBUpdate);
+
         rq.setResponse(emptyResponse.getDefaultInstance());
+    }
+
+    private void logXLocInfo(XLocList xLocList) {
+        StringBuilder replicaInfo = new StringBuilder();
+        for (int i = 0; i < xLocList.getReplicaCount(); i++) {
+            replicaInfo
+                    .append("replica ")
+                    .append(i)
+                    .append(" osd: ")
+                    .append(xLocList.getReplica(i)
+                                    .getOSD(0))
+                    .append(" replication flags: ")
+                    .append(xLocList.getReplica(i)
+                                    .getReplicationFlags())
+                    .append("\n");
+        }
+        Logging.logMessage(Logging.LEVEL_DEBUG, this,
+                           replicaInfo.toString());
     }
 }
